d8b1dd8b608ed33cd4f1cf0c933ea32a7ae7ea05
samsilversteinn <sesilversteinn@gmail.com>
2024-05-03 14:44:08 -0400

    done

--- main.js
+++ main.js
@@ -5,11 +5,10 @@ const app = Vue.createApp({
       product: 'Socks',
       brand: 'Vue Mastery',
       selectedVariant: 0,
-      inStock: false,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
       ]
     }
   },
@@ -22,14 +21,14 @@ const app = Vue.createApp({
     }
   },
   computed: {
-    title (){
-      return this.band + ' '+ this.product
+    title() {
+      return this.brand + ' ' + this.product
     },
     image(){
-      return this.variant[this.selectedVariant].image
+      return this.variants[this.selectedVariant].image
     },
-    inStock (){
-      return this.variant[this.selectedVariant].quality
+    inStock(){
+      return this.variants[this.selectedVariant].quantity
     }
   }
 })
